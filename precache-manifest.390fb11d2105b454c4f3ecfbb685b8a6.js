ec08747a70d838535ab138547a16b2fe554b29d4
Sergey Mirasov <sergeymirasov@protonmail.com>
2020-09-09 21:03:04 +0300

    Updates

---
+++ precache-manifest.390fb11d2105b454c4f3ecfbb685b8a6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9ddeb0332cb67f84cd1a2ea2da65de",
+    "url": "/vezdekod-web-mini-app/index.html"
+  },
+  {
+    "revision": "010ac615897b4bb52d04",
+    "url": "/vezdekod-web-mini-app/static/css/main.24a2169f.chunk.css"
+  },
+  {
+    "revision": "1b5a6f1537dc5cac661c",
+    "url": "/vezdekod-web-mini-app/static/js/2.938fedf1.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/vezdekod-web-mini-app/static/js/2.938fedf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010ac615897b4bb52d04",
+    "url": "/vezdekod-web-mini-app/static/js/main.5cb2c966.chunk.js"
+  },
+  {
+    "revision": "1f0de3f823c2835fd946",
+    "url": "/vezdekod-web-mini-app/static/js/runtime-main.df4545d2.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/vezdekod-web-mini-app/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/vezdekod-web-mini-app/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/vezdekod-web-mini-app/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/vezdekod-web-mini-app/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/vezdekod-web-mini-app/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/vezdekod-web-mini-app/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/vezdekod-web-mini-app/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/vezdekod-web-mini-app/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/vezdekod-web-mini-app/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/vezdekod-web-mini-app/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/vezdekod-web-mini-app/static/media/sub.4eb05fdd.png"
+  }
+]);
